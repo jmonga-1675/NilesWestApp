32bc40c05b9b8d34acb354484d0262ca184170d6
jaythemaster <jaymon1@nilesk12.org>
2017-09-29 15:31:47 -0500

    Made app launchable and did stuff with schedule view

--- app/src/main/java/org/jamango/nileswestapp/Schedule.java
+++ app/src/main/java/org/jamango/nileswestapp/Schedule.java
@@ -2,6 +2,7 @@ package org.jamango.nileswestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class Schedule extends AppCompatActivity {
@@ -10,9 +11,12 @@ public class Schedule extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+        WebView webview = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.loadUrl("https://studio.code.org/projects/applab/i664H2YZ3mipSrMB_x_XQA");
     }
 
-    WebView webview = (WebView) findViewById(R.id.webView);
-    webview.
+
 
 }
