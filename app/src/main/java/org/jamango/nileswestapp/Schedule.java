4e5153888134bde0bcfa9d5861cdd809667196ff
jaythemaster <jaymon1@nilesk12.org>
2017-09-29 13:54:49 -0500

    Add view to activity and add activity for schedule view

---
+++ app/src/main/java/org/jamango/nileswestapp/Schedule.java
@@ -0,0 +1,18 @@
+package org.jamango.nileswestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class Schedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+
+    WebView webview = (WebView) findViewById(R.id.webView);
+    webview.
+
+}
